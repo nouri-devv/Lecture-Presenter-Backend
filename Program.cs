e52cb753a4fc9aae7349cd0786f001512b963872
Mostafa Nouri <nouri.mst06@outlook.com>
2025-04-04 15:28:02 +1100

    Add file upload functionality and configure VSCode settings

--- Program.cs
+++ Program.cs
@@ -1,12 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-const string filePath = "data.txt";
-const string filePath2 = "data2.txt";
-const string filePath3 = "data3.txt";
-const string filePath4 = "data4.txt";
-const string filePath5 = "data5.txt";
+// Map controllers
+app.MapControllers();
 
-app.MapGet("/", () => "Hello World!");
 
-app.Run();
+app.Run();app.Run();
