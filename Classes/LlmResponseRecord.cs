bdf59305480677beef4a620d7527ee98beafe942
Mostafa Nouri <nouri.mst06@outlook.com>
2025-05-07 15:32:58 +1000

    Rename the typo for LlmResponseRecord

--- Classes/LlmResponseRecord.cs
+++ Classes/LlmResponseRecord.cs
@@ -1,13 +1,13 @@
 public class LlmResponseRecord
 {
     public string ResponseId { get; set; }
-    public string ReppnseHeading { get; set; }
+    public string ResponseHeading { get; set; } // <- typo fixed
     public string ResponseExplanation { get; set; }
 
-    public LlmResponseRecord(string responseId, string reppnseHeading, string responseExplanation)
+    public LlmResponseRecord(string responseId, string responseHeading, string responseExplanation)
     {
         ResponseId = responseId;
-        ReppnseHeading = reppnseHeading;
+        ResponseHeading = responseHeading;
         ResponseExplanation = responseExplanation;
     }
 }
